019660e0bbf90f0056fc0141bbc2ea12493f8322
Ebunoluwa Amoo <ebunoluwa.amoo@kibo.school>
2024-10-22 23:54:07 +0100

    [top-nav] - add active property to links

--- podcast-landing-page/src/components/Navbar.tsx
+++ podcast-landing-page/src/components/Navbar.tsx
@@ -1,19 +1,42 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
-import echoLens from '../../public/echolensLogo.png'
+import echoLens from '../../public/echolensLogo.png';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar: React.FC = () => {
+    const pathname = usePathname();
+    const isActive = (path: string) => pathname === path;
+
     return (
         <nav className="flex justify-between items-center py-4 bg-accent_white px-6">
             <div className="text-main_black text-xl font-bold flex">
                 <Image src={echoLens} alt='Echolens logo' className='mr-2'/>
                 Echolens
             </div>
-            <ul className="flex space-x-6 text-accent_gray ml-6">
-                <li><a href="/" className='text-main_orange font-medium'>Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/episodes">Episodes</a></li>
-                <li><a href="/contact">Blog</a></li>
+            <ul className="flex space-x-6 ml-6">
+                <li>
+                    <Link href="/" className={`${isActive('/') ? 'text-main_orange font-medium' : 'text-accent_gray font-normal'}`}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about" className={`${isActive('/about') ? 'text-main_orange font-medium' : 'text-accent_gray font-normal'}`}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/episodes" className={`${isActive('/episodes') ? 'text-main_orange font-medium' : 'text-accent_gray font-normal'}`}>
+                        Episodes
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/blog" className={`${isActive('/blog') ? 'text-main_orange font-medium' : 'text-accent_gray font-normal'}`}>
+                        Blog
+                    </Link>
+                </li>
             </ul>
             <div className='space-x-3 flex ml-2'>
                 <button className='border-main_orange py-2 px-6 border text-main_orange rounded-md'>
